6004fcd7839350175960714f0804762e62c06477
Roman Ivanov <58957111+ROcket-DIma@users.noreply.github.com>
2022-04-03 23:01:42 +0500

    Add files via upload

---
+++ VS_DLL/DLL.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "DLL.h"
+#include <locale.h>
+#include "iostream"
+
+int __stdcall number()
+{
+	static int n = 571227;
+	return n;
+}
+
+void __stdcall fio()
+{
+	setlocale(LC_ALL, "rus");
+	const char* a = "Иванов Роман Сергеевич";
+	std::cout << a;
+}
---
+++ VS_DLL/DLL.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#ifndef DLL_EXPORTS
+#define DLL_API __declspec(dllexport)
+#else
+#define DLL_API __declspec(dllimport)
+#endif
+
+
+extern "C" DLL_API int __stdcall number();
+extern "C" DLL_API void __stdcall fio();
