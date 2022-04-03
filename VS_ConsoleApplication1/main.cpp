a4db80e9eef72227a75a390b44eb2a38b02f9d05
Roman Ivanov <58957111+ROcket-DIma@users.noreply.github.com>
2022-04-03 23:09:17 +0500

    Add files via upload

---
+++ VS_ConsoleApplication1/main.cpp
@@ -0,0 +1,16 @@
+﻿//Выполнил студент группы РИ-571227 Иванов Роман
+
+#pragma comment(lib, "C:\\Users\\admin\\Desktop\\LR1_Ivanov\\VisualStudio\\VS_DLL\\x64\\Debug\\DLL\\Dll.lib") //автоматически залинкует библиотеку
+#include <iostream>
+#include <locale.h>
+#include "DLL.h"
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Лабораторная работа номер 1:\nработа с динамическими библиотеками DLL\nВыполнил следующий студент:\n";
+    std::cout << "Номер группы: " << number() << "\n";
+    std::cout << "ФИО: ";
+    fio();
+    system("pause");
+}
